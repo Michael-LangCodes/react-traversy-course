3a27ea11b7ec6713a22730aa97bdb9b31b65b8f6
MichaelCodes <michael.lang3303@gmail.com>
2026-03-19 20:49:58 -0500

    Home Cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import HomeCards from './components/HomeCards'
 
 const App = () => {
   return (
     <>
     <Navbar />
-
     <Hero />
-
-    {/* <!-- Developers and Employers --> */}
+    <HomeCards />
     <section className="py-4">
       <div className="container-xl lg:container m-auto">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
