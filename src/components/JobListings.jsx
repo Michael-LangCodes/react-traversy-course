327be0d3a7307c558f0a50e7bed7a1ac3eda127a
MichaelCodes <michael.lang3303@gmail.com>
2026-03-22 13:28:32 -0500

    Loading

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -30,9 +30,13 @@ const JobListings = ({isHome=false}) => {
           {isHome ? 'Recent Jobs' : 'Browse Jobs' }
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          {jobs.map((job)=>(
-            <JobListing key = {job.id} job={job} />
-          ))}
+          { loading ? (<h2>Loading...</h2>) : (
+            <>
+              {jobs.map((job)=>(
+                <JobListing key = {job.id} job={job} />
+              ))}
+            </>
+          )}
           
 
         </div>
